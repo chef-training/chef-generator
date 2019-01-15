6e321334fa4db5a2bb38884184e439385d2dc795
Kimball Johnson <drrk@users.noreply.github.com>
2019-01-15 17:58:26 +0000

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
-name             'code_generator'
-description      'Custom code generator cookbook for use with ChefDK'
-long_description 'Custom code generator cookbook for use with ChefDK'
+name             'generator'
+description      'Custom code generator cookbook for use with Chef Workstation'
+long_description 'This alters the .kitchen.yml files to generate working kitchen-dokken configs for the Chef Training Classes'
 version          '0.1.0'
 
